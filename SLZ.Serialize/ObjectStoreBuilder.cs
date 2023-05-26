130a4bef21603f016a9958be1a94538271e6812b
yewnyx <yewnyx@yewnyx.xyz>
2023-05-25 18:09:52 -0700

    Bump version and fix up the builder

--- SLZ.Serialize/ObjectStoreBuilder.cs
+++ SLZ.Serialize/ObjectStoreBuilder.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json.Linq;
 
 namespace SLZ.Serialize {
     public partial class ObjectStore {
-        public static ObjectStoreBuilder builder() => new ObjectStoreBuilder();
+        public static ObjectStoreBuilder Builder => new ObjectStoreBuilder();
         public ref struct ObjectStoreBuilder {
             private IEnumerable<KeyValuePair<Type, string>> _builtinTypes;
             private IEnumerable<KeyValuePair<Type, string>> _types;
@@ -14,32 +14,38 @@ namespace SLZ.Serialize {
             private ISet<IPackable> _objectSet;
             private JObject _jsonDocument;
 
-            private ObjectStoreBuilder WithBuiltInTypes(IEnumerable<KeyValuePair<Type, string>> builtinTypes) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithBuiltInTypes(IEnumerable<KeyValuePair<Type, string>> builtinTypes) {
                 _builtinTypes = builtinTypes;
                 return this;
             }
             
-            private ObjectStoreBuilder WithTypes(IEnumerable<KeyValuePair<Type, string>> types) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithTypes(IEnumerable<KeyValuePair<Type, string>> types) {
                 _types = types;
                 return this;
             }
 
-            private ObjectStoreBuilder WithTypeRenames(IEnumerable<KeyValuePair<string, string>> typeRenames) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithTypeRenames(IEnumerable<KeyValuePair<string, string>> typeRenames) {
                 _typeRenames = typeRenames;
                 return this;
             }
 
-            private ObjectStoreBuilder WithObjects(IEnumerable<KeyValuePair<string, IPackable>> objects) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithObjects(IEnumerable<KeyValuePair<string, IPackable>> objects) {
                 _objects = objects;
                 return this;
             }
 
-            private ObjectStoreBuilder WithObjectSet(ISet<IPackable> objectSet) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithObjectSet(ISet<IPackable> objectSet) {
                 _objectSet = objectSet;
                 return this;
             }
 
-            private ObjectStoreBuilder WithJsonDocument(JObject jsonDocument) {
+            [PublicAPI]
+            public ObjectStoreBuilder WithJsonDocument(JObject jsonDocument) {
                 _jsonDocument = jsonDocument;
                 return this;
             }
@@ -47,12 +53,12 @@ namespace SLZ.Serialize {
             [PublicAPI]
             public ObjectStore Build() => 
                 new ObjectStore(
-                    _builtinTypes,
-                    _types,
-                    _typeRenames,
-                    _objects,
-                    _objectSet,
-                    _jsonDocument);
+                    _builtinTypes ?? new Dictionary<Type, string>(),
+                    _types ?? new Dictionary<Type, string>(),
+                    _typeRenames ?? new Dictionary<string, string>(),
+                    _objects ?? new Dictionary<string, IPackable>(),
+                    _objectSet ?? new HashSet<IPackable>(),
+                    _jsonDocument ?? new JObject());
         }
     }
 }
